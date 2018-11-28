15859712ada3352ac9bbee752a4c37d0053ec700
andynines <ams975@drexel.edu>
2018-11-28 00:01:32 -0500

    Removed vital stats

--- virtual-pet.js
+++ virtual-pet.js
@@ -7,29 +7,7 @@ Drexel CS164
 
 
 // Constants
-var MAX_HAPPINESS = 300;
-var MAX_HUNGER = 300;
-var MAX_FATIGUE = 300;
-
-var DEC_HAPPINESS = 1;
-var DEC_HUNGER = 3;
-var DEC_FATIGUE = 2;
-
 var TICK = 1000; // Update delay (ms) resets if got input in meantime
-
-// Housekeeping vars
-var timer;
-
-var stateIndex;
-var totalStates;
-var totalStimuli;
-
-// Vital vars
-var happiness;
-var hunger;
-var fatigue;
-
-
 
 // Possible states
 var states = [
@@ -80,8 +58,6 @@ var states = [
         img: "special-wheel.png"
     }
 ];
-
-
 
 // Available user inputs
 var stimuli = [
@@ -135,6 +111,15 @@ var stimuli = [
 
 
 
+// Housekeeping vars
+var timer;
+
+var stateIndex;
+var totalStates;
+var totalStimuli;
+
+
+
 function getNamedElement(list, name) {
 
     // Locate item with "name" property in list (states, stimuli)
@@ -172,22 +157,11 @@ function draw() {
     img.alt = descText;
     out.innerHTML = descText;
 
-    // Debug
-    console.log(
-        "hap " + happiness.toString() +
-        "\nhun " + hunger.toString() +
-        "\nfat " + fatigue.toString()
-    );
-
 }
 
 
 
 function update() {
-
-    happiness = Math.min(MAX_HAPPINESS, Math.max(0, happiness - DEC_HAPPINESS));
-    hunger = Math.min(MAX_HUNGER, Math.max(0, hunger - DEC_HUNGER));
-    fatigue = Math.min(MAX_FATIGUE, Math.max(0, fatigue - DEC_FATIGUE));
 
     // set new state
 
@@ -203,15 +177,7 @@ function doAction(actionName) {
 
     clearTimeout(timer);
 
-    var fx;
-    var vitalDeltas;
-    var deltaIndex;
-
-    fx = getNamedElement(stimuli, actionName).effect;
-
-    happiness += (fx.deltaHappiness != undefined)? fx.deltaHappiness : 0;
-    hunger += (fx.deltaHunger != undefined)? fx.deltaHunger : 0;
-    fatigue += (fx.deltaFatigue != undefined)? fx.deltaFatigue : 0;
+    // logic
 
     update();
 
@@ -242,10 +208,6 @@ function init() {
     stateIndex = 0;
     totalStates = states.length;
     totalStimuli = stimuli.length;
-
-    happiness = MAX_HAPPINESS;
-    hunger = MAX_HUNGER;
-    fatigue = MAX_FATIGUE;
 
     buildInputs();
     draw();
